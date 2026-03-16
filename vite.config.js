039b4430c95a21d8b85b00d1f695813baea6dee1
Chris Nova <mgbenyapeter@gmail.com>
2026-03-16 13:15:22 +0100

    update base path

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Ensure react plugin is imported
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // Ensure react plugin is imported
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: "/NovaTrack/",
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       // Forwarding requests from /api-gecko to CoinGecko to bypass CORS
-      '/api-gecko': {
-        target: 'https://api.coingecko.com/api/v3',
+      "/api-gecko": {
+        target: "https://api.coingecko.com/api/v3",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-gecko/, ''),
+        rewrite: (path) => path.replace(/^\/api-gecko/, ""),
       },
     },
   },
-})
-
+});
